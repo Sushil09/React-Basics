911e7d09499208bab6d036ed3b698f416e342fe5
Sushil <sjsushil09@gmail.com>
2023-05-01 00:17:20 +0530

    JSX and component based rendering

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1"
+const heading = (
+  <h1 id="title" key="h1">
+    Namastey React
+  </h1>
 );
-
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "Container",
-  },
-  [heading, heading2]
-);
-
-// console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(heading);
